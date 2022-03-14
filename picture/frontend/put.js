abf40ac9e9f4146cfaeae98983f6b6f521145dcc
siva kumar <99823143+SaiSivaKumar-0906@users.noreply.github.com>
2022-03-14 21:45:27 +0530

    Update put.js

--- picture/frontend/put.js
+++ picture/frontend/put.js
@@ -21,18 +21,17 @@ const videos = document.getElementById("#screenshot-video");
 
 const canvas = document.createElement("canvas");
 
-captureVideoButton.onclick = function () {
-  navigator.mediaDevices
-    .getUserMedia(constraints)
-    .then(handleSuccess)
-    .catch(handleError);
-};
+// captureVideoButton.onclick = function () {
+//   navigator.mediaDevices
+//     .getUserMedia(constraints)
+//     .then(handleSuccess)  
+//     .catch(handleError);
+// };
 
 screenshotButton.onclick = videos.onclick = function () {
   canvas.width = videos.videoWidth;
   canvas.height = videos.videoHeight;
   canvas.getContext("2d").drawImage(videos, 0, 0);
-  // Other browsers will fall back to image/png
   img.src = canvas.toDataURL("image/webp");
 };
 
