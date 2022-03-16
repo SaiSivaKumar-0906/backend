111c5d3c16607bdabb61bdcb0ecf5ce7029e531b
siva kumar <99823143+SaiSivaKumar-0906@users.noreply.github.com>
2022-03-16 16:22:08 +0530

    Update express.js

--- picture/express.js
+++ picture/express.js
@@ -2,18 +2,30 @@ const express = require('express')
 
 const put = express()
 
-
 const port = 6586
+
+const path = require('path')
+
+const multer = require('multer')
+
+const storeage = multer.diskStorage({
+    destination : (one, File, back)=>{
+        back(null, "serverphotos")
+    },
+    filename: (reslu, File, cd)=>{
+        console.log(File)
+        cd(null, Date.now() + path.extname(File.originalname))
+    }
+})
+
+const upload = multer({storage: storeage})
 
 put.use('/frontend', express.static('frontend'))
 put.use(express.json())
 
-put.get('/user', (request, responce)=>{
-   responce.send('photos')
-})
 
 
-put.post('/data', (req, res)=>{
+put.post('/data', upload.single('images') ,(req, res)=>{
     console.log(req.body)
 })
 
