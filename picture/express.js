282aeb285b5721bcf2a24a7ab3e7a02caa9e2c65
siva kumar <99823143+SaiSivaKumar-0906@users.noreply.github.com>
2022-03-30 18:08:33 +0530

    Update express.js

--- picture/express.js
+++ picture/express.js
@@ -8,22 +8,6 @@ const path = require("path")
 
 const multer = require('multer')
 
-const moongoose = require("mongoose")
-
-const fs = require('fs')
-
-moongoose.connect("mongodb://localhost:27017/number-one-images")
-
-const images = moongoose.Schema(
-    {
-       images : {
-           data : Buffer,
-           contentType : String
-       }
-    }
-)
-
-const modelss = moongoose.model("test", images)
 
 
 const imgstore = multer.diskStorage({
@@ -41,12 +25,8 @@ const imgrec = multer({storage : imgstore})
 put.use('/one', express.static('one'))
 put.use(express.json())
 
-put.post('/data', imgrec.single('images'), async(req, res)=>{
-     const newItem = new modelss;
-     newItem.images.data = fs.readFileSync(req.files.serverphotos.path)
-     newItem.images.contentType = 'image/png'
-     newItem.save();
-     res.send("uploaded")
+put.post('/data', imgrec.single('images'), (req, res)=>{
+   res.send("uploaded")
 })
 
 put.listen(port, ()=>{
