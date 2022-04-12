353a331e078e4800c71075f20b07211c467fb410
siva kumar <99823143+SaiSivaKumar-0906@users.noreply.github.com>
2022-04-12 19:07:02 +0530

    Update login.js

--- chat/login.js
+++ chat/login.js
@@ -10,14 +10,39 @@ const models = require("./models/moongose")
 
 const bycrypt = require("bcryptjs")
 
+const jwt = require("jsonwebtoken")
+
+const JWTSCERET = "sdfoajsdifasdfoisdjfisdkj#ksdfjoasdfkljdsjj@!djoajfijadsjdsoi"
 
 app.use("/frontend", express.static("frontend"))
 
 app.use(express.json())
 
+app.post("/api/login", async(req, res)=>{
+    const {username, password} = req.body;
+
+    const db = await models.findOne({username}).lean()
+
+    if(!db){
+        return res.json({status : "erorr", error : "username/password might be wrong!!"})
+    }
+
+    if(await  bycrypt.compare(password, db.password)){
+        const token = jwt.sign({
+             id : models._id , 
+             username : models.username  }, JWTSCERET) 
+
+        return res.json({status : "ok", data : token})
+    }
+    res.json({status : "erorr", error : "username/password might be wrong!!"})
+
+})
+
 app.post("/server/get-data", async (req, res)=>{
     const {username, password : letters}= req.body
+
     const password = await bycrypt.hash(letters, 10)
+
     if(!username || typeof username !== "string"){
           return res.json(
               {status : 'error', 
