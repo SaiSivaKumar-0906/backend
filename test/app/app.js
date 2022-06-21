0b887b4daca72e55514b1de2758640aa46078930
siva kumar <99823143+SaiSivaKumar-0906@users.noreply.github.com>
2022-06-21 21:54:44 +0530

    Update app.js

--- test/app/app.js
+++ test/app/app.js
@@ -6,13 +6,17 @@ const input = document.getElementById("text");
 button.addEventListener("click", function(){
     const ws = new WebSocket("ws://localhost:8080");
     ws.onopen = ()=>{
-        ws.send(input.value);
+        ws.send(JSON.stringify(input.value));
     }
-    ws.binaryType = "blob";
+//     ws.binaryType = "blob";
     ws.onmessage = async (event)=>{
-      const buffer = event.data;
-      const blob = new Blob([buffer], {type : 'text/plain; charset=uft-8'});
-      const one =  blob.text().then(siva => console.log(siva));
-      header.innerHTML = one;
+        //receiving binary data;
+//       const buffer = event.data;
+//       const blob = new Blob([buffer], {type : 'text/plain; charset=uft-8'});
+//       const one = await  blob.text();
+        
+        //receiving json data;
+         const name = prompt("enter your name");
+         header.innerHTML = `${name} says: ${event.data}`;
     }
 })
