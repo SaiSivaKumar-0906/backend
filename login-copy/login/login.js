d51d86ba1faf98606c6873d06ae4a5e9f28ddb97
siva kumar <99823143+SaiSivaKumar-0906@users.noreply.github.com>
2022-07-18 17:50:52 +0530

    Update login.js

--- login-copy/login/login.js
+++ login-copy/login/login.js
@@ -1,14 +1,21 @@
-const input = document.getElementById("input").value
-const button = document.getElementById("button")
+let submit = document.getElementById("submit")
 
-button.onclick = async function(){
-        await fetch("/post/data", {
+let result;
+
+submit.onclick = async function(){
+    
+    let username = document.getElementById("username").value
+
+    let password = document.getElementById("password").value
+
+     result = await fetch("/post/data", {
         method : "POST",
         headers : {
-            'Content-Type':'application/json',
+            'Content-Type':'application/json'
         },
         body : JSON.stringify({
-            input
+            username,
+            password
         }),
        
     }).then((e)=>console.log(e));
