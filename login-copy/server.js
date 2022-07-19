03d4959e260abc28ee2941273c8f911f5043efd9
siva kumar <99823143+SaiSivaKumar-0906@users.noreply.github.com>
2022-07-19 17:02:28 +0530

    Update server.js

--- login-copy/server.js
+++ login-copy/server.js
@@ -1,19 +1,22 @@
-const express = require('express');
+const http = require("http");
 
-const app =express();
-
-app.use(express.json());
-
-app.post("/post/data", (req, res)=>{
-    const {username, password} = req.body;
-    if(!username || !password){
-        res.status(400).send({mssage:"error", error:"password or username wrong"})
+const app = http.createServer(async(req, res)=>{
+    const postData = [];
+    if(req.url === "/post/data" && req.method === "POST"){
+    for await(const chunk of req){
+        postData.push(chunk)
+    }
+    const {username, password} = JSON.parse(Buffer.concat(postData).toString());
+    if(!(username && password)){
+        res.write(JSON.stringify({error: "wirte username or password"}))
     }else{
-        res.status(200).send({message:"successfully created"})
+        res.write(JSON.stringify({message : "succesfully created"}))
     }
     console.log({username, password})
-});
+    res.end();
+}
+})
 
 app.listen(9966, ()=>{
-    console.log("9966")
+    console.log(9966)
 })
