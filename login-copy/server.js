39f1f42ea1505a3f39468c95bcd0d70859685744
siva kumar <99823143+SaiSivaKumar-0906@users.noreply.github.com>
2022-07-28 16:53:49 +0530

    Update server.js

--- login-copy/server.js
+++ login-copy/server.js
@@ -2,14 +2,15 @@ const http = require("http");
 
 const fs = require("fs")
 
+const path = require("path")
+
 const mongoose = require("mongoose");
 
 mongoose.connect("mongodb://127.0.0.1/live-login");
 
 const models = require("./models");
 
-const app = http.createServer(async(req, res)=>{ 
-    
+const app = http.createServer(async(req, res)=>{    
     const postData = [];
 
     if(req.url === "/api/login" && req.method === "POST"){
@@ -20,7 +21,7 @@ const app = http.createServer(async(req, res)=>{
     const {username, password} = JSON.parse(Buffer.concat(postData).toString());
 
     if(!(username&&password)){
-        res.write(JSON.stringify({message:"you might be forgot to write username or password, check it again"}))
+        res.write(JSON.stringify({message:"you must be forgot to write username or password"}));
     }
     
     try{
@@ -30,27 +31,28 @@ const app = http.createServer(async(req, res)=>{
             password
         })
          if(db === username,password){
-            res.write(JSON.stringify({messgae:"account created"}))
+            res.end(JSON.stringify({messgae:"account created"}))
             console.log({username, password}, db)
         }
     }else{
-        res.write(JSON.stringify({message:"special symbols not allowed"}))
+        res.end(JSON.stringify({message:"special symbols not allowed"}))
     }
     }
     catch(error){
         if(error.code === 11000){
-            res.write(JSON.stringify({message:"username already taken"}))
+            res.end(JSON.stringify({message:"username already taken"}))
         }
     }
 }
-    fs.readFile(`${__dirname}/login/index.html`, (err, data)=>{
+    if(req.url === "/login" && req.method === "GET"){
+    fs.readFile(path.join(`${__dirname}/login/index.html`), (err, data)=>{
         if(err){
                 console.log(err)
         }else{
                 res.end(data)
         }
     })
-
+}
 })
 
 app.listen(9966, ()=>{
