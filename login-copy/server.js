09c601071227d2e5c64ef923164607f4fddda859
siva kumar <99823143+SaiSivaKumar-0906@users.noreply.github.com>
2022-07-29 17:05:20 +0530

    Update server.js

--- login-copy/server.js
+++ login-copy/server.js
@@ -10,10 +10,10 @@ mongoose.connect("mongodb://127.0.0.1/live-login");
 
 const models = require("./models");
 
-const app = http.createServer(async(req, res)=>{    
+const app = http.createServer(async(req, res)=>{  
     const postData = [];
 
-    if(req.url === "/api/login" && req.method === "POST"){
+    if(req.url === "/api/create" && req.method === "POST"){
     for await(const bufferData of req){
         postData.push(bufferData)
     } 
@@ -21,30 +21,39 @@ const app = http.createServer(async(req, res)=>{
     const {username, password} = JSON.parse(Buffer.concat(postData).toString());
 
     if(!(username&&password)){
-        res.write(JSON.stringify({message:"you must be forgot to write username or password"}));
+        res.end(JSON.stringify({oops:"you must be forgot to write username or password"}));
     }
     
     try{
-        if(/^[A-Za-z0-9 -]*$/.test(username)){
+        if(/^[A-Za-z0-9]*$/.test(username)){
         const db = await models.create({
             username,
             password
         })
          if(db === username,password){
-            res.end(JSON.stringify({messgae:"account created"}))
+            res.end(JSON.stringify({messgae:"account created, now login"}))
             console.log({username, password}, db)
         }
     }else{
-        res.end(JSON.stringify({message:"special symbols not allowed"}))
+        res.end(JSON.stringify({oops:"special symbols and spaces are not allowed"}))
     }
     }
     catch(error){
         if(error.code === 11000){
-            res.end(JSON.stringify({message:"username already taken"}))
+            res.end(JSON.stringify({oops:"username already taken"}))
         }
     }
+} 
+if(req.url ==="/login" && req.method === "GET"){
+    fs.readFile(path.join(`${__dirname}/login/login.html`), (err, data)=>{
+        if(err){
+            console.log(err)
+        }else{
+            res.end(data)
+        }
+    })
 }
-    if(req.url === "/login" && req.method === "GET"){
+    if(req.url==="/create" && req.method === "GET"){
     fs.readFile(path.join(`${__dirname}/login/index.html`), (err, data)=>{
         if(err){
                 console.log(err)
@@ -55,6 +64,6 @@ const app = http.createServer(async(req, res)=>{
 }
 })
 
-app.listen(9966, ()=>{
-    console.log("http://192.168.0.129:9966")
+app.listen(80, ()=>{
+    console.log("http://192.168.0.129:80")
 })
