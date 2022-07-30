f84c56fc80330aea3e14305d9373d33970a2b856
siva kumar <99823143+SaiSivaKumar-0906@users.noreply.github.com>
2022-07-30 18:36:33 +0530

    Update server.js

--- login-copy/server.js
+++ login-copy/server.js
@@ -14,45 +14,55 @@ const app = http.createServer(async(req, res)=>{
     const postData = [];
 
     if(req.url === "/api/create" && req.method === "POST"){
-    for await(const bufferData of req){
-        postData.push(bufferData)
-    } 
+        for await(const bufferData of req){
+            postData.push(bufferData)
+        } 
 
     const {username, password} = JSON.parse(Buffer.concat(postData).toString());
 
-    if(!(username&&password)){
-        res.end(JSON.stringify({oops:"you must be forgot to write username or password"}));
-    }
-    
+        if(!(username&&password)){
+            res.end(JSON.stringify({oops:"you must be forgot to write username or password"}));
+        }
+        
     try{
-        if(/^[A-Za-z0-9]*$/.test(username)){
-        const db = await models.create({
+        if(/^[A-Za-z0-9 -]*$/.test(username)){
+            const db = await models.create({
             username,
             password
         })
-         if(db === username,password){
+        if(db === username,password){
             res.end(JSON.stringify({messgae:"account created, now login"}))
             console.log({username, password}, db)
         }
-    }else{
-        res.end(JSON.stringify({oops:"special symbols and spaces are not allowed"}))
-    }
-    }
-    catch(error){
-        if(error.code === 11000){
-            res.end(JSON.stringify({oops:"username already taken"}))
-        }
-    }
-} 
-if(req.url ==="/login" && req.method === "GET"){
-    fs.readFile(path.join(`${__dirname}/login/login.html`), (err, data)=>{
-        if(err){
-            console.log(err)
         }else{
-            res.end(data)
+            res.end(JSON.stringify({oops:"special symbols and spaces are not allowed"}))
         }
-    })
-}
+        }
+        catch(error){
+            if(error.code === 11000){
+                res.end(JSON.stringify({oops:"username already taken"}))
+            }
+        }
+    } 
+
+
+
+    if(req.url === "/api/login" && req.method === "POST"){
+        for await(const bufferData of req){
+            postData.push(bufferData)
+        }
+        const {username, password} = JSON.parse(Buffer.concat(postData).toString());
+        const user = await models.findOne({username, password})
+        if(!user){
+            res.end(JSON.stringify({messge :"successfully loged in"}))
+        }else{
+            res.end(JSON.stringify({message:"account found"}))
+        }
+    }
+
+
+
+
     if(req.url==="/create" && req.method === "GET"){
     fs.readFile(path.join(`${__dirname}/login/index.html`), (err, data)=>{
         if(err){
@@ -61,8 +71,17 @@ if(req.url ==="/login" && req.method === "GET"){
                 res.end(data)
         }
     })
-}
-})
+    }
+    if(req.url==="/login" && req.method === "GET"){
+    fs.readFile(path.join(`${__dirname}/login/login.html`), (err, data)=>{
+        if(err){
+            console.log(err)
+        }else{
+            res.end(data)
+        }
+    })
+   }
+})  
 
 app.listen(80, ()=>{
     console.log("http://192.168.0.129:80")
