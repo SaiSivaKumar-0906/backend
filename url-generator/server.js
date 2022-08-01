aed0b5dfcf58ff20da6e159a2218107e483e97fc
siva kumar <99823143+SaiSivaKumar-0906@users.noreply.github.com>
2022-08-01 18:05:09 +0530

    Add files via upload

---
+++ url-generator/server.js
@@ -0,0 +1,35 @@
+const http = require("http");
+const fs = require("fs")
+const nodemailer = require("nodemailer");
+
+const app = http.createServer(async(req, res)=>{
+
+    let regex = new RegExp("([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\"\(\[\]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\[[\t -Z^-~]*])");
+    
+    const postData = [];
+    
+    if(req.url === "/send-email" && req.method === "POST"){
+    for await(const chunk of req){
+        postData.push(chunk)
+    }
+    const {mail} = JSON.parse(Buffer.concat(postData).toString());
+
+    if(!regex.test(mail)){
+       return res.end(JSON.stringify("write the correct email address like example@gmail.com"))
+    }
+}
+
+    if(req.url === "/get-mail" && req.method === "GET"){
+        fs.readFile(`${__dirname}/ui/mail.html`, (err, data)=>{
+            if(err){
+                console.log(err)
+            }else{
+                res.end(data)
+            }
+        })
+    }
+});
+
+app.listen(80, ()=>{
+    console.log(`http://192.168.0.129`)
+})
