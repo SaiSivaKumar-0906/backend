b591083b0bd3740acafdc7c7bf7c749caeb80ce3
siva kumar <99823143+SaiSivaKumar-0906@users.noreply.github.com>
2022-08-02 18:29:18 +0530

    Update server.js

--- url-generator/server.js
+++ url-generator/server.js
@@ -1,6 +1,13 @@
+require("dotenv").config()
 const http = require("http");
 const fs = require("fs")
 const nodemailer = require("nodemailer");
+const {google} = require("googleapis");
+
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URI = process.env.REDIRECT_URI;
+const REFRESH_TOKEN = process.env.REFRESH_TOKEN;
 
 const app = http.createServer(async(req, res)=>{
 
@@ -19,22 +26,59 @@ const app = http.createServer(async(req, res)=>{
 
     if(!regex.test(mail)){
        return res.end(JSON.stringify("write the correct email address like example@gmail.com"))
-    }else{
-        res.end(JSON.stringify("check your email"))
-    }
-}
+    }   
+    const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI)
+    oAuth2Client.setCredentials({refresh_token: REFRESH_TOKEN})
 
-    if(req.url === "/get-mail" && req.method === "GET"){
-        fs.readFile(`${__dirname}/ui/mail.html`, (err, data)=>{
-            if(err){
-                console.log(err)
-            }else{
-                res.end(data)
-            }
-        })
+async function sendMail(){
+  try{
+    const accessToken = await oAuth2Client.getAccessToken()
+
+    const transport = nodemailer.createTransport({
+        service: 'gmail',
+
+        auth: {
+            type: 'OAuth2',
+            user: 'webdev0906@gmail.com',
+            clientId: CLIENT_ID,
+            clientSecret: CLIENT_SECRET,
+            refreshToken : REFRESH_TOKEN,
+            accessToken : accessToken
+        }
+    })
+    const mailOptions = {
+        from : 'webdev0906@gmail.com',
+        to : mail,
+        subject : "will it work",
+        text : "hey bro did you saw my mail"
     }
+
+    const result = await transport.sendMail(mailOptions);
+    return result;
+
+  }catch(err){
+    if(err){
+        console.log(err)
+    }
+  }
+}
+res.end("check mail")
+
+
+sendMail().then(res => console.log(res)).catch(err => console.log(err.message))
+     
+    
+}  
+
+    fs.readFile(`${__dirname}/ui/mail.html`, (err, data)=>{
+        if(err){
+            console.log(err)
+        }else{
+            res.end(data)
+        }
+    })
 });
 
 app.listen(80, ()=>{
-    console.log(`http://192.168.0.129`)
+    console.log(`http://192.168.0.105/get-mail`)
 })
