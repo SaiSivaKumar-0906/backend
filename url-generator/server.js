08f2589c632583f27e8832f9c58985b6b39624e8
siva kumar <99823143+SaiSivaKumar-0906@users.noreply.github.com>
2022-08-08 17:47:53 +0530

    Update server.js

--- url-generator/server.js
+++ url-generator/server.js
@@ -3,10 +3,10 @@ const fs = require("fs")
 const nodemailer = require("nodemailer");
 const {google} = require("googleapis");
 
-const CLIENT_ID = '106400557797-d8hmi9j0vb5sk3b168n2had7muoblg69.apps.googleusercontent.com';
-const CLIENT_SECRET = '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';
-const REDIRECT_URI = 'https://developers.google.com/oauthplayground';
-const REFRESH_TOKEN = '1//04Vb3iSaGqSvPCgYIARAAGAQSNwF-L9IrJvwbITuUpy6z4iWYi2xeCUDzCsl5kA3uET8B17R0D0gc_L0xeaBm0TloD5NIASFuQbM';
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URI = process.env.REDIRECT_URI;
+const REFRESH_TOKEN = process.env.REFRESH_TOKEN;
 
 const random = Math.floor(Math.random()*256)
 
@@ -27,7 +27,8 @@ const app = http.createServer(async(req, res)=>{
 
     if(!regex.test(mail)){
        return res.end(JSON.stringify("write the correct email address like example@gmail.com"))
-    }   
+    }  
+        
     const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI)
 
     oAuth2Client.setCredentials({refresh_token: REFRESH_TOKEN})
