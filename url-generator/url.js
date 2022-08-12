b9d33a878c77e191061515ca268a845148245aaa
siva kumar <99823143+SaiSivaKumar-0906@users.noreply.github.com>
2022-08-12 18:31:48 +0530

    Update url.js

--- url-generator/url.js
+++ url-generator/url.js
@@ -1,27 +1,9 @@
-const fs = require("node:fs");
-
-const http = require("node:http");
-
 const url = require("node:url");
 
+let videoCount = (Math.random() + 1).toString(36).substring(7);
 
-const myUrl = url.parse(`http://192.168.0.105/video/something`);
+console.log(videoCount)
 
-
-const app = http.createServer((req, res)=>{
-    if(req.url === myUrl.href && req.method === "GET"){
-        fs.readFile(`${__dirname}ui/create.html`, (err, data)=>{
-            if(err){
-                console.log(err)
-            }else{
-                res.end(data)
-            }
-        })
-    }
-})
-
-app.listen(81, ()=>{
-    console.log(81)
-})
+const myUrl = url.parse(`http://192.168.0.105/video/stream-${videoCount}`);
 
 module.exports.url = myUrl;
