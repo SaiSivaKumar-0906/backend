7be784b140e76abefab7a9559f4a47d237208afd
siva kumar <99823143+SaiSivaKumar-0906@users.noreply.github.com>
2022-08-13 17:18:00 +0530

    Add files via upload

---
+++ url-generator/db/models.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const models = mongoose.Schema({
+    mail : {
+        type:String,
+        require:true
+    },
+    urlPathname:{
+        type:String,
+        require:true
+    }
+})
+const Schema = mongoose.model("host-info", models);
+
+module.exports = Schema;
