d321864c5d127a264c6b86e0c021ca657e7f6c15
siva kumar <99823143+SaiSivaKumar-0906@users.noreply.github.com>
2022-08-15 18:01:26 +0530

    Delete url.js

--- url-generator/url.js
+++
@@ -1,9 +0,0 @@
-const url = require("node:url");
-
-let videoCount = (Math.random() + 1).toString(36).substring(7);
-
-console.log(videoCount)
-
-const myUrl = url.parse(`http://192.168.0.105/video/stream-${videoCount}`);
-
-module.exports.url = myUrl;
