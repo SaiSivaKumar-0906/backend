01ff20f7afb0214c305e3dcf90e42762265aff1d
siva kumar <99823143+SaiSivaKumar-0906@users.noreply.github.com>
2022-09-17 16:54:29 +0530

    Update signallingServer.mjs

--- url-generator/signallingServer.mjs
+++ url-generator/signallingServer.mjs
@@ -6,6 +6,7 @@ mongoose.connect("mongodb://127.0.0.1/mailId-url")
 const fs = require("node:fs");
 const { WebSocketServer } = require("ws");
 
+
 const app = http.createServer(async(req, res)=>{
     if(await db.findOne({urlPathname: req.url}) && req.method === "GET"){
        fs.readFile(`${__dirname}/public/create.html`, (err, data)=>{
@@ -18,15 +19,16 @@ const app = http.createServer(async(req, res)=>{
     }
 })
 
-const wss = new WebSocketServer({port:8080})
+const wss = new WebSocketServer({port: 8080})
 
 wss.on("connection", (ws)=>{
    console.log("user connected");
 
    ws.on("message", (data)=>{
-      console.log("message %s", data);
+       let {one} = JSON.parse(data.toString())
+      console.log({one});
+      ws.send(JSON.stringify({one}))
    })
-   ws.send("shit")
 })
 
 
@@ -34,3 +36,4 @@ wss.on("connection", (ws)=>{
 app.listen(9966, ()=>{
     console.log(8080)
 })
+
