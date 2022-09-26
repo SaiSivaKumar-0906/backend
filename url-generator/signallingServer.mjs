2743e4531dd17facb06fcae3acefb4dd17b8ad35
siva kumar <99823143+SaiSivaKumar-0906@users.noreply.github.com>
2022-09-26 21:59:09 +0530

    Update signallingServer.mjs

--- url-generator/signallingServer.mjs
+++ url-generator/signallingServer.mjs
@@ -2,9 +2,11 @@ const os = require("node:os")
 const http = require("node:http");
 const mongoose = require("mongoose");
 const db = require("./db/models")
+const socketMessages = require("./db/sockets")
 mongoose.connect("mongodb://127.0.0.1/mailId-url")
 const fs = require("node:fs");
-const {WebSocketServer}, WebSocket = require("ws")
+const {WebSocketServer} = require("ws");
+const WebSocket = require("ws");
 
 
 const app = http.createServer(async(req, res)=>{   
@@ -23,28 +25,42 @@ const wss = new WebSocketServer({
    server: app
 })
 
-wss.on("connection", (ws)=>{
-   console.log("user connected");
+const clients = new Array();
 
-   ws.on("message", (data, isBinary)=>{
+function connections(client){ 
+
+   clients.push(client);
+
+   function endClient(){
+      const removedClient = clients.indexOf(client);
+      clients.splice(removedClient, 1);
+      console.log("connection closed");
+   }
+
+   async function responce(data){
+      const datas = JSON.parse(data)
+      brodcast(JSON.parse(data));
       try{
-         const {input} = JSON.parse(data);
-
-         console.log({input})
-
-         for(const client of wss.clients){
-            if(client.readyState === WebSocket.OPEN){
-               client.send(JSON.stringify({
-                  input
-               }), {binary: isBinary})
-            }
-         }
+         const happens = await socketMessages.create({
+            data
+         })
       }catch(e){
-         ws.send(JSON.stringify("server has blown away for some reason"))
+         console.log(e)
       }
-   })
-})
+   }
 
+   client.on('message', responce);
+   client.on('close', endClient);
+}
+
+
+function brodcast(data){
+   for(c in clients){
+      clients[c].send(JSON.stringify(data))
+   }
+}
+
+wss.on("connection", connections)
 
 app.listen(9966, ()=>{
    console.log(9966)
