fbe5bf5dada67e8b572fc666b62311d34bdac688
siva kumar <99823143+SaiSivaKumar-0906@users.noreply.github.com>
2022-10-01 15:20:24 +0530

    Update signallingServer.mjs

--- url-generator/signallingServer.mjs
+++ url-generator/signallingServer.mjs
@@ -2,8 +2,6 @@ const os = require("node:os")
 const http = require("node:http");
 const mongoose = require("mongoose");
 const db = require("./db/models")
-const socketMessages = require("./db/sockets")
-mongoose.connect("mongodb://127.0.0.1/mailId-url")
 const fs = require("node:fs");
 const {WebSocketServer} = require("ws");
 const WebSocket = require("ws");
@@ -20,39 +18,6 @@ const app = http.createServer(async(req, res)=>{
       })  
    }
 })
-
-const wss = new WebSocketServer({
-   server: app
-})
-
-const clients = new Array();
- function connections(client){ 
-
-   clients.push(client);
-
-   function endClient(){
-      const removedClient = clients.indexOf(client);
-      clients.splice(removedClient, 1);
-      console.log("connection closed");
-   }
-
-   async function responce(data){
-      console.log(JSON.parse(data))
-      brodcast(JSON.parse(data));
-   }
-
-   client.on('message', responce);
-   client.on('close', endClient);
-}
-
-
-function brodcast(data){
-   for(c in clients){
-      clients[c].send(JSON.stringify(data))
-   }
-}
-
-wss.on("connection", connections)
 
 
 app.listen(9966, ()=>{
