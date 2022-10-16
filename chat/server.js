c2887ede7d5617e47ce7b8d087df8fed63bdc410
siva kumar <99823143+SaiSivaKumar-0906@users.noreply.github.com>
2022-10-16 18:44:28 +0530

    Update server.js

--- chat/server.js
+++ chat/server.js
@@ -1,56 +1,65 @@
-const http = require("node:http");
-const { WebSocketServer } = require("ws");
-const ws = require("ws")
-const WebSocket = require("ws");
-const fs = require("node:fs")
+  const http = require("node:http");
+  const { WebSocketServer } = require("ws");
+  const ws = require("ws")
+  const WebSocket = require("ws");
+  const fs = require("node:fs")
 
-const app  = http.createServer((req, res)=>{
-  if(req.url === "/" && req.method === "GET"){
-    fs.readFile(`${__dirname}/public/index.html` , (err, data)=>{
-      try{
-        res.end(data)
-      }catch{
-        throw err
-      }
-    })
-  } 
-  if(req.url === "/api" && req.method === "GET"){
-     res.writeHead(302, {
-      "Location": "https://www.example.com"
-    })
-    res.end();
-  }
-});
-
-const wss = new WebSocketServer({
-   server: app
-})
-
-const array = new Array();
-
-wss.on("connection", (ws)=>{
-  array.push(ws)
-  ws.on("message", (data)=>{
-    const {message} = JSON.parse(data)
-    wss.clients.forEach((client)=>{
-      if(client.readyState === WebSocket.OPEN){
-        client.send(JSON.stringify(message))
-      }      
-    })
-    ws.on("close", removedclinet)
-  })
-})
-
-
-function removedclinet(client){
-  for(let i=0; i<array.length; i++){
-    if(array[i] === client){
-      array.splice(i, 1)
+  const app  = http.createServer((req, res)=>{
+    if(req.url === "/" && req.method === "GET"){
+      fs.readFile(`${__dirname}/public/index.html` , (err, data)=>{
+        try{
+          res.end(data)
+        }catch{
+          throw err
+        }
+      })
+    } 
+    if(req.url === "/apis" && req.method === "GET"){
+      res.writeHead(302, {
+        "Location": "/public/index.html"
+      })
+      res.end()
     }
-  }
-  console.log("disconnected")
-}
+    if(req.url === "/public/index.html" && req.method === "GET"){
+      fs.readFile(`${__dirname}/public/websocket.html`, (err, data)=>{
+        if(err){
+          console.log(err)
+        }else{
+          res.end(data)
+        }
+      })
+    }
+  });
 
-app.listen(8080, ()=>{
-    console.log(80)
-})
+  const wss = new WebSocketServer({
+    server: app
+  })
+
+  const array = new Array();
+
+  wss.on("connection", (ws)=>{
+    array.push(ws)
+    ws.on("message", (data)=>{
+      const {message} = JSON.parse(data)
+      wss.clients.forEach((client)=>{
+        if(client.readyState === WebSocket.OPEN){
+          client.send(JSON.stringify(message))
+        }      
+      })
+      ws.on("close", removedclinet)
+    })
+  })
+
+
+  function removedclinet(client){
+    for(let i=0; i<array.length; i++){
+      if(array[i] === client){
+        array.splice(i, 1)
+      }
+    }
+    console.log("disconnected")
+  }
+
+  app.listen(8080, ()=>{
+      console.log(80)
+  })
