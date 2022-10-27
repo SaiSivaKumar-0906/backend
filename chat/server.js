00add038679f92e65126142421f8042385a0ce0b
siva kumar <99823143+SaiSivaKumar-0906@users.noreply.github.com>
2022-10-27 22:55:13 +0530

    Update server.js

--- chat/server.js
+++ chat/server.js
@@ -90,3 +90,10 @@ const app  = http.createServer(async(req, res)=>{
 app.listen(8080, ()=>{
   console.log(80)
 })
+
+
+async function willTry(){
+  return console.log(await db.findOne({url: "users/10059481-b9f6-4200-9b48-7d229c8359ab"}));
+} 
+
+console.log(willTry().then(e=>e))
