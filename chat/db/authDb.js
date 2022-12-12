f9c64a62fb7b428f40d9b4693e09e1be4059cb30
siva kumar <99823143+SaiSivaKumar-0906@users.noreply.github.com>
2022-12-12 13:51:45 +0530

    Add files via upload

---
+++ chat/db/authDb.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const models = mongoose.Schema({
+    username:{
+        type: String,
+        require: true,
+        unique: true
+    },
+    password:{
+        type: String, 
+        require: true
+    }
+})
+
+const dbs = mongoose.model("atuh", models);
+module.exports.db = dbs
