8ea80949a55c30401b1447853d9dd870e36edccf
siva kumar <99823143+SaiSivaKumar-0906@users.noreply.github.com>
2023-01-29 20:34:44 +0530

    Update server.js

--- cli./server.js
+++ cli./server.js
@@ -1,11 +1,12 @@
 const http = require("node:http");
 const post = require("./http-methods/methods").post;
 
-
-http.createServer((req, res)=>{
+const app = http.createServer((req, res)=>{
     if(req.method === "POST"){
         post(req, res)
     }
-}).listen(200, ()=>{
+})
+
+app.listen(200, ()=>{
     console.log(200)
 })
