2fc302fa41ed781ef59e4631dbcaf1922f1106ee
siva kumar <99823143+SaiSivaKumar-0906@users.noreply.github.com>
2023-01-29 20:34:06 +0530

    Update methods.js

--- cli./http-methods/methods.js
+++ cli./http-methods/methods.js
@@ -1,32 +1,31 @@
 const headersPost = {
-  "two-hundread": 200,
-  "four-O-four": 404,
+  statuscodes : [200, 401],
   content: function(n){
     const obj = {
-        "Content-Type" : n
+      "Content-Type" : n
     }
     return obj;
   }
 }
+
 const created = "got the data!!";
 const fourOfour = "you should write the content!!!"
 
 async function post(req, res){
-    const postData = [];
-    for await(const data of req){
-        console.log(data);
-        postData.push(data);
-    }
-    const {recivedData} = JSON.parse(Buffer.concat(postData).toString());
-    if(recivedData){
-        res.writeHead(headersPost["two-hundread"],  headersPost.content("application/json"));
-        res.write(JSON.stringify(created));
-        res.end();
-    }else{
-       res.writeHead(headersPost["four-O-four"], headersPost.content("application/json"));
-       res.write(JSON.stringify(fourOfour));
-       res.end();   
-    }
+  const postData = [];
+  for await(const data of req){
+    postData.push(data);
+  }
+  const {recivedData} = JSON.parse(Buffer.concat(postData).toString());
+  if(recivedData){
+    res.writeHead(headersPost.statuscodes[0],  headersPost.content("application/json"));
+    res.write(JSON.stringify(created));
+    res.end();
+  } else{
+    res.writeHead(headersPost.statuscodes[1], headersPost.content("application/json"));
+    res.write(JSON.stringify(fourOfour));
+    res.end();   
+  }
 }
 
 module.exports.post = post;
