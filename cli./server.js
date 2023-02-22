1b3eb9bb89503895740d12a96b3f8bfb04a198af
siva kumar <99823143+SaiSivaKumar-0906@users.noreply.github.com>
2023-02-22 19:01:56 +0530

    Update server.js

--- cli./server.js
+++ cli./server.js
@@ -1,13 +1,46 @@
-const http = require("node:http");
-const {Post} = require("./http-methods/methods")
+const { randomFillSync } = require('crypto');
+const fs = require('node:fs');
+const http = require('http');
+const os = require('os');
+const path = require('path');
+const busboy = require('busboy');
 
+const random = (() => {
+  const buf = Buffer.alloc(16);
+  return () => randomFillSync(buf).toString('hex');
+})();
 
-const app = http.createServer((req, res)=>{
-    if(req.method === "POST"){
-        Post(req, res);
-    }
+const app = http.createServer((req, res) => {
+  if(req.method === "GET"){
+    fs.readFile(`${__dirname}/hate.html`, (err, data)=>{
+      if(err){
+        throw err;
+      }else{
+        res.writeHead(200, {
+          "Content-Type": "text/html"
+        })
+        res.write(data);
+        res.end();
+      }
+  })
+  }
+  if (req.method === 'POST') {
+    const bb = busboy({ headers: req.headers });
+    bb.on('file', (name, file, info) => {
+      const extension = path.extname(info.filename);
+      console.log(extension, info);
+      const saveTo = path.join(__dirname, `busboy-upload-${random()}.${extension}`);
+      file.pipe(fs.createWriteStream(saveTo));
+    });
+    bb.on('close', () => {
+      res.writeHead(200, {
+        "Content-Type": 'application/json'
+      });
+      res.end(JSON.stringify(`That's all folks!`));
+    });
+    req.pipe(bb);
+  }
 })
 
-app.listen(80, ()=>{
-    console.log(80)
-})
+app.listen(8000)
+
