5fbc487a45211f8b7f0edbbed07db75f5eab83a1
siva kumar <99823143+SaiSivaKumar-0906@users.noreply.github.com>
2023-04-21 21:09:49 +0530

    Update signaling.js

--- livestream/live-stream/signaling-server/signaling.js
+++ livestream/live-stream/signaling-server/signaling.js
@@ -1,21 +1,19 @@
 const ws = require("ws");
 const fs = require("fs");
-const arrayTwo = [];
-const {Readable} = require("node:stream");
+const webM = require("../s").sussy
 
 function httpServer(HtToWs){
     const wss = new ws.WebSocketServer({
         server: HtToWs
     })
     wss.on("connection", (ws)=>{    
-        ws.on("message", function(data){
-            arrayTwo.push(data);
+        ws.on("message", (data)=>{
+            webM(data)
         });
     })
 }
 
-if(arrayTwo.length > 1){
-    console.log(arrayTwo);
-}
+
+
 
 module.exports.hTows = httpServer
